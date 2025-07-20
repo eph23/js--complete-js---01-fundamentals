857ce6f0d53140874c81c5d9af03e414ec25b926
ephraim <ephraims.818@gmail.com>
2025-07-20 06:17:32 +0600

    JS--COMPLETE-JS---FUNDAMENTALS - 08. Introduction to Objects

---
+++ 08. Introduction to Objects/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Object Literal
+const eph = {
+    firstName: "Ephraim",
+    lastName: "Sangma",
+    age: 2037 - 1988,
+    job: "Web-Dev",
+    friends: ["Michael", "Steve", "Jay"],
+};
